2bc6aa8c34ea51afcd77bb143ff698f1a0936b66
robotLJW <790504160@qq.com>
2020-10-14 09:14:27 +0800

    ac problem 24

---
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.go
@@ -0,0 +1,27 @@
+package _4_swap_nodes_in_pairs
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func swapPairs(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	tempHead := &ListNode{
+	}
+	tempHead.Next = head
+	var swap func(pre, end, leftNode, RightNode *ListNode)
+	swap = func(pre, end, leftNode, RightNode *ListNode) {
+		pre.Next = RightNode
+		RightNode.Next = leftNode
+		leftNode.Next = end
+	}
+	temp := tempHead
+	for temp.Next != nil && temp.Next.Next != nil {
+		swap(temp, temp.Next.Next.Next, temp.Next, temp.Next.Next)
+		temp=temp.Next.Next
+	}
+	return tempHead.Next
+}
--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # leetcode-golang
 leetcode个人刷题记录
 
+[24.两两交换链表中的节点](24-swap-nodes-in-pairs/swap-nodes-in-pairs.go)
+
 [75.颜色分类](75-sort-colors/sort-colors.go)
 
-[344.反转字符串](344-reverse-string/reverse-string.go)
-
 [141.环形链表](141-linked-list-cycle/linked-list-cycle.go)
+
+[344.反转字符串](344-reverse-string/reverse-string.go)
 
 [530.二叉搜索树的最小绝对差](530-minimum-absolute-difference-in-bst/minimum-absolute-difference-in-bst.go)
